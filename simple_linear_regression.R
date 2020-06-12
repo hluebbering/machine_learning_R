937c91ff5b7eb2717efb874fea87bd8c2839cb91
Hannah <luebhr17@wfu.edu>
2020-06-11 22:30:24 -0600

    update

---
+++ simple_linear_regression.R
@@ -0,0 +1,40 @@
+# Simple Linear Regression
+
+dataset = read.csv('Salary_Data.csv')
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fitting Simple Linear Regression to Training set
+regressor = lm(formula = Salary ~ YearsExperience, data = training_set)
+
+# Predicting Test set results
+y_pred = predict(regressor, newdata = test_set)
+
+# Visualizing Training set results
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (Training set)') +
+  xlab('Years of experience') +
+  ylab('Salary')
+
+# Visualizing Test set results
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (Test set)') +
+  xlab('Years of experience') +
+  ylab('Salary')
