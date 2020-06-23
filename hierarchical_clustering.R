05545319cb6e5f5a4ff661ca48d113c50ec6323a
Hannah <luebhr17@wfu.edu>
2020-06-23 00:03:26 -0500

    Hierarchical Clustering

---
+++ hierarchical_clustering.R
@@ -0,0 +1,43 @@
+# Hierarchical Clustering
+
+# Importing dataset
+dataset = read.csv('Mall_Customers.csv')
+dataset = dataset[4:5]
+
+# Splitting dataset into Training set and Test set
+# install.packages('caTools')
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Using dendrogram to find optimal number of clusters
+dendrogram = hclust(d = dist(dataset, method = 'euclidean'), method = 'ward.D')
+plot(dendrogram,
+     main = paste('Dendrogram'),
+     xlab = 'Customers',
+     ylab = 'Euclidean distances')
+
+# Fitting Hierarchical Clustering to dataset
+hc = hclust(d = dist(dataset, method = 'euclidean'), method = 'ward.D')
+y_hc = cutree(hc, 5)
+
+# Visualizing the clusters
+library(cluster)
+clusplot(dataset,
+         y_hc,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels= 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of customers'),
+         xlab = 'Annual Income',
+         ylab = 'Spending Score')
+
