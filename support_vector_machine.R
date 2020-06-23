8366fef4d74fc3e8ff1360793844b140a621c2f1
Hannah <luebhr17@wfu.edu>
2020-06-22 23:43:50 -0500

    support vector machine

--- support_vector_machine.R
+++ support_vector_machine.R
@@ -34,7 +34,7 @@ y_pred = predict(classifier, newdata = test_set[-3])
 cm = table(test_set[, 3], y_pred)
 
 
-# Visualising the Training set results
+# Visualizing the Training set results
 library(ElemStatLearn)
 set = training_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
@@ -51,7 +51,7 @@ points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
 
 
-# Visualising the Test set results
+# Visualizing the Test set results
 library(ElemStatLearn)
 set = test_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
